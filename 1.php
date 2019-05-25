bba01e1226f979ee7ae316684dd98551aa2738c1
Nindy5 <nindyraisahanum@gmail.com>
2019-05-25 15:00:16 +0700

    Update 1.php

--- 1.php
+++ 1.php
@@ -1,18 +1,31 @@
 <?php
-  function name 
-           address
-           hobbies
-           is_married
-           school
-           skills
-           
-   return $name;
-          $address;
-          hobbies;
-          is_married;
-          school;
-          skills;
-    echo "Nama : . $name;
-    echo "Address : . $address;
-    echo "
+  function getNama(){
+      var nama = $('#nama').val():
+      return nama;
+  }
+
+  function getAddress(){
+      var address = $('#address').val():
+      return address;
+  }
+
+  function getHobbies(){
+       array Hobbies = $('#hobbies').val():
+       return Hobbies
+  }
+
+  function getMarried(){
+       var boolean = $('#married').val():
+       return is_married;
+  }
+
+  function getSchool(){
+      var school = $('#school').val():
+      return School;
+  }
+
+  function getSkills(){
+       var address = $('#name', '#score').val():
+       return Address;
+  }
  ?>
