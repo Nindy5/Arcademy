02e8ce23453790e76d3d4353bbcafecd7d1775c4
Nindy5 <nindyraisahanum@gmail.com>
2019-05-25 17:00:11 +0700

    Update 6.sql

--- 6.sql
+++ 6.sql
@@ -1,3 +1,16 @@
-Create table;
+Create table users
+{
+ID varchar (20) not null;
+name varchar (50) not null;
+};
 
+Create table skills
+{
+ID varchar (20) not null;
+name varchar (50) not null;
+user_id varchar (20) not null;
+};
+
+select name, skills 
+from user inner join skills on id.users = id.skills;
 
