0a75537f14a46b978cab21e389ebf450b95877af
Nindy5 <nindyraisahanum@gmail.com>
2019-05-25 12:29:35 +0700

    Create 1.php

---
+++ 1.php
@@ -0,0 +1,18 @@
+<?php
+  function name 
+           address
+           hobbies
+           is_married
+           school
+           skills
+           
+   return $name;
+          $address;
+          hobbies;
+          is_married;
+          school;
+          skills;
+    echo "Nama : . $name;
+    echo "Address : . $address;
+    echo "
+ ?>
